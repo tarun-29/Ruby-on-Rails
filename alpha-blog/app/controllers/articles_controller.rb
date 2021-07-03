b1b2394b5d76982bd4379a19a23a01f7f81048d3
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-03 11:38:47 +0530

    fix create and falash and error and show

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -6,8 +6,19 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    # @article.save
+    # redirect_to article_show(@article)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    
   end
 
   private
