46ede8e4fd1a84738043005d4ab8b1d05077f5bf
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-03 15:58:26 +0530

    delete article controller

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -48,6 +48,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
