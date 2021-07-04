eb10c0daf5b24599586b527b58a4f458568c9f3c
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-04 14:39:33 +0530

    user sign up

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :articles
-  before_save = {self.email = email.downcase}
+  before_save {self.email = email.downcase}
   validates :username, presence: true,
             uniqueness: {case_sensitive: false},
             length: {minimum: 3, maximum: 25}
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_075604) do
+ActiveRecord::Schema.define(version: 2021_07_04_084517) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_075604) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
