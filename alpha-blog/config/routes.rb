c407b709a3520e0d30ff2ab00a99dbbc52f218a0
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-04 19:47:58 +0530

    half done

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
