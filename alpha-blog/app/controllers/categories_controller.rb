2bd39f0465cb96b27fe155425c4715dffa8d7f0a
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-05 21:00:10 +0530

    category test implementation

---
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -0,0 +1,61 @@
+# class CategoriesController < ApplicationController
+
+#   def index
+#     @categories = Category.all
+#   end
+
+#   def new
+#     @category = Category.new
+#   end
+
+#   def create
+#     @category = Category.new(category_params)
+#     if @category.save
+#       flash[:success] = "Categories created succesfully"
+#       redirect_to categories_path
+#     else
+#       render 'new'
+#     end
+#   end
+
+#   def show
+
+#   end
+
+#   private
+#   def category_params
+#     params.require(:category).permit(:name)
+#   end
+
+# end
+
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+  
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
