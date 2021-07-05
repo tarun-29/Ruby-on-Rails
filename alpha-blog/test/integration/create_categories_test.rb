f2e5d6b9f58bd9b40c8f4ee62f9c190dd79d04e6
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-05 21:32:27 +0530

    admin requirement to edit category

--- alpha-blog/test/integration/create_categories_test.rb
+++ alpha-blog/test/integration/create_categories_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = User.create(username: "johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@user, "password")
+  end
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
