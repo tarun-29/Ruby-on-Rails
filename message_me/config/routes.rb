c164fbe94ff6057333ba6a9616cc595114a4a87f
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2021-07-11 02:01:42 +0530

    finsih message app

--- message_me/config/routes.rb
+++ message_me/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- message_me/db/seeds.rb
+++ message_me/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Tarun", password: "password")
+User.create(username: "Uday", password: "password")
+User.create(username: "Satwik", password: "password")
+User.create(username: "Omnish", password: "password")
+User.create(username: "Harman", password: "password")
